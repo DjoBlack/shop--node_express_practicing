956a2cdb813bc7061b7094bf39fb04f0eaa0f870
Vladimir Garmash <djonblack.mail@gmail.com>
2023-06-22 16:54:58 +0300

    fixed issue where model using deleted utils folder

--- models/product.js
+++ models/product.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-const rootDir = require('../util/path.js');
 
-const p = path.join(rootDir, 'data', 'product.json');
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'product.json');
 
 const getProductsFromFile = cb => {
         fs.readFile(p, (err, fileContent) => {
